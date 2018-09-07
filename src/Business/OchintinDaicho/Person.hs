e4202d3a21eb2289359e9b058f26932f7484aa0c
Kadzuya Okamoto <arow.okamoto@gmail.com>
2018-09-07 19:41:09 +0900

    Add Person.markdown

--- src/Business/OchintinDaicho/Person.hs
+++ src/Business/OchintinDaicho/Person.hs
@@ -15,9 +15,12 @@ This module exports core functions and types for payroll books.
 module Business.OchintinDaicho.Person
   -- * Pretty printers
   ( ppr
+  , markdown
   ) where
 
 import Business.OchintinDaicho (Person(..))
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
 ppr :: Person -> IO ()
@@ -30,3 +33,19 @@ ppr Person {..} = do
   T.putStrLn $ "退職または死亡年月日: " <> left
   T.putStrLn $ "退職または死亡の理由: " <> reason
   T.putStrLn $ "履歴: " <> history
+
+
+markdown :: Person -> Text
+markdown Person {..} =
+  T.unlines
+    [ "## 氏名: " <> name
+    , ""
+    , "* 生年月日: " <> birthday
+    , "* 性別: " <> sex
+    , "* 住所: " <> address
+    , "* 業務の種類: " <> job
+    , "* 退職または死亡年月日: " <> left
+    , "* 退職または死亡の理由: " <> reason
+    , "* 履歴: " <> history
+    ]
+
